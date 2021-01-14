bda51e8a1f783d66fca4dcb4c179a40b49a67cdb
lixia <xautlx@hotmail.com>
2021-01-14 21:09:10 +0800

    Client重连机制

--- entdiy-nat-client/src/main/java/com/entdiy/nat/client/listener/NatClientListener.java
+++ entdiy-nat-client/src/main/java/com/entdiy/nat/client/listener/NatClientListener.java
@@ -24,6 +24,8 @@ import com.entdiy.nat.common.codec.NatMessageDecoder;
 import com.entdiy.nat.common.codec.NatMessageEncoder;
 import com.entdiy.nat.common.constant.Constant;
 import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.ChannelPipeline;
@@ -39,39 +41,89 @@ import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import io.netty.handler.timeout.IdleStateHandler;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 @Slf4j
 public class NatClientListener {
 
+    /**
+     * 连接失败重连间隔秒数
+     */
+    private static final int RECONNECT_INTERVAL_SECONDS = 30;
+
+    /**
+     * 失败连接次数累加器，连接成功后重置0
+     */
+    private static int connectTimes = 0;
+
+    /**
+     * 重连定时作业服务
+     */
+    private ScheduledExecutorService pool = null;
+
     private static final EventLoopGroup group = new NioEventLoopGroup();
     private static Bootstrap b = new Bootstrap();
+
+    static {
+        b.group(group)
+                .channel(NioSocketChannel.class)
+                .option(ChannelOption.TCP_NODELAY, true)
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline p = ch.pipeline();
+                        //    p.addLast(sslCtx.newHandler(ch.alloc()));
+                        p.addLast(new LoggingHandler());
+                        p.addLast(new IdleStateHandler(60, 80, 120));
+                        p.addLast(new DelimiterBasedFrameDecoder(10240, Constant.DELIMITER));
+                        p.addLast(new NatMessageDecoder());
+                        p.addLast(new NatMessageEncoder());
+                        p.addLast(new ClientControlHandler());
+                    }
+                });
+    }
+
 
     public void run() {
         NatClientConfigProperties config = ClientContext.getConfig();
         try {
             SslContext sslCtx = SslContextBuilder.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE)
                     .build();
-            b.group(group)
-                    .channel(NioSocketChannel.class)
-                    .option(ChannelOption.TCP_NODELAY, true)
-                    .handler(new ChannelInitializer<SocketChannel>() {
-                        @Override
-                        public void initChannel(SocketChannel ch) throws Exception {
-                            ChannelPipeline p = ch.pipeline();
-                            //    p.addLast(sslCtx.newHandler(ch.alloc()));
-                            p.addLast(new LoggingHandler());
-                            p.addLast(new IdleStateHandler(60, 80, 120));
-                            p.addLast(new DelimiterBasedFrameDecoder(10240, Constant.DELIMITER));
-                            p.addLast(new NatMessageDecoder());
-                            p.addLast(new NatMessageEncoder());
-                            p.addLast(new ClientControlHandler());
-                        }
-                    });
-
-            log.debug("Start connect to server {}:{}", config.getServerAddr(), config.getPort());
-            b.connect(config.getServerAddr(), config.getPort()).sync();
+            if (connectTimes == 0) {
+                log.debug("Start connect to server {}:{}", config.getServerAddr(), config.getPort());
+            } else {
+                log.debug("Start {} times reconnect to server {}:{}", connectTimes, config.getServerAddr(), config.getPort());
+            }
+            connectTimes++;
+            ChannelFuture f = b.connect(config.getServerAddr(), config.getPort()).sync();
+            f.addListener((ChannelFutureListener) future -> {
+                if (future.isSuccess()) {
+                    log.info("Success connect to server {}:{}", config.getServerAddr(), config.getPort());
+                    connectTimes = 0;
+                } else {
+                    log.warn("Fail connect to server {}:{}", config.getServerAddr(), config.getPort());
+                    scheduleReconnect();
+                }
+            });
+            f.channel().closeFuture().addListener((ChannelFutureListener) t -> {
+                log.warn("Disconnect to server {}:{}", config.getServerAddr(), config.getPort());
+                scheduleReconnect();
+            });
         } catch (Exception e) {
-            log.error("Connect to server failure {}:{}", config.getServerAddr(), config.getPort(), e);
+            log.warn("Exception connect to server {}:{}", config.getServerAddr(), config.getPort());
+            log.debug("Exception connect to server", e);
+            scheduleReconnect();
         }
+    }
+
+    private void scheduleReconnect() {
+        if (pool == null) {
+            pool = Executors.newScheduledThreadPool(1);
+        }
+        log.warn("Schedule to reconnect after {} seconds", RECONNECT_INTERVAL_SECONDS);
+        pool.schedule(() -> run(), RECONNECT_INTERVAL_SECONDS, TimeUnit.SECONDS);
     }
 
     public void shutdown() {
